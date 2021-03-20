9b82ace04e228f7837c84b4b749bfe2c6a053681
forest1040 <forest1040@gmail.com>
2021-03-21 08:58:40 +0900

    add 描画デバッグログ

--- src/components/BoardContent.tsx
+++ src/components/BoardContent.tsx
@@ -43,13 +43,14 @@ const BoardContent: React.FC = () => {
   };
 
   const renderLists = () => {
+    //console.log("renderLists");
     const id = boardIdNumber();
     const result = store.allLists
       .filter((list) => list.boardId === id)
       .sort((a, b) => a.index - b.index)
       .map((list, listIndex) => {
         console.log("list.id:" + list.id);
-        console.log("listIndex:" + listIndex);
+        //console.log("listIndex:" + listIndex);
         if (!list.id) {
           return <></>;
         }
